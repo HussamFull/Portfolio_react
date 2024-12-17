f552ed3a13077844fe4e3e35f1dccc08b99ca444
hamada <bhak6150@gmail.com>
2024-12-17 19:53:46 +0100

    Portfolio

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -30,7 +30,7 @@ export default function Footer() {
           <h4 className="text-uppercase mb-4">About Freelancer</h4>
           <p className="lead mb-0">
             Freelance is a free to use, MIT licensed Bootstrap theme created by
-            <a href="http://startbootstrap.com">Start Bootstrap</a>
+            <a href="#">Start Portfolio</a>
             .
           </p>
         </div>
